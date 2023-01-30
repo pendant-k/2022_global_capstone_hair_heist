5d1395984bcef2fa61ba2e637d3549d7cc4570b2
ParkJihwan <aptheparker@gmail.com>
2023-01-30 17:06:07 +0900

    favorite list changed

--- lib/app/ui/like/view/like_page.dart
+++ lib/app/ui/like/view/like_page.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 // configs
 import 'package:hair_heist/app/config/palette.dart';
 import 'package:hair_heist/app/config/global_styles.dart';
+import 'package:hair_heist/app/ui/detail/view/detail_page.dart';
 
 class LikePage extends StatelessWidget {
   const LikePage({super.key});
@@ -18,202 +19,11 @@ class LikePage extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(20.w, 39.w, 20.w, 0),
         child: ListView(
           children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              height: 120.w,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    //Image
-                    width: 100.w,
-                    height: 100.w,
-                    color: Colors.green,
-                    child: Text('Hair Style Image'),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hair Name',
-                        style: GlobalStyle.primaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      Text(
-                        'By: Styler Name',
-                        style: GlobalStyle.secondaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.favorite),
-                      Text('1002'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              height: 120.w,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    //Image
-                    width: 100.w,
-                    height: 100.w,
-                    color: Colors.green,
-                    child: Text('Hair Style Image'),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hair Name',
-                        style: GlobalStyle.primaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      Text(
-                        'By: Styler Name',
-                        style: GlobalStyle.secondaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.favorite),
-                      Text('1002'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              height: 120.w,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    //Image
-                    width: 100.w,
-                    height: 100.w,
-                    color: Colors.green,
-                    child: Text('Hair Style Image'),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hair Name',
-                        style: GlobalStyle.primaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      Text(
-                        'By: Styler Name',
-                        style: GlobalStyle.secondaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.favorite),
-                      Text('1002'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              height: 120.w,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    //Image
-                    width: 100.w,
-                    height: 100.w,
-                    color: Colors.green,
-                    child: Text('Hair Style Image'),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hair Name',
-                        style: GlobalStyle.primaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      Text(
-                        'By: Styler Name',
-                        style: GlobalStyle.secondaryText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.favorite),
-                      Text('1002'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            FavoriteList(),
+            FavoriteList(),
+            FavoriteList(),
+            FavoriteList(),
+            FavoriteList(),
           ],
         ),
       ),
@@ -223,3 +33,100 @@ class LikePage extends StatelessWidget {
 
 // TODO: Home Banner position
 // TODO: Hint text 내용 수정
+
+class FavoriteList extends StatelessWidget {
+  const FavoriteList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(4.0, 4.0),
+              blurRadius: 15.0,
+              spreadRadius: 1.0,
+            ),
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-4.0, -4.0),
+              blurRadius: 15.0,
+              spreadRadius: 1.0,
+            ),
+          ],
+        ),
+        height: 120.w,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              //Image
+              width: 100.w,
+              height: 100.w,
+              child: Center(
+                child: Text('Hair Style Image'),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(4.0, 4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hair Name',
+                  style: GlobalStyle.primaryText.copyWith(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.sp,
+                  ),
+                ),
+                Text(
+                  'By: Styler Name',
+                  style: GlobalStyle.secondaryText.copyWith(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.sp,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.favorite),
+                Text('1002'),
+              ],
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        Get.to(DetailPage());
+      },
+    );
+  }
+}
